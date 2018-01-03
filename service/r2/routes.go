e4dd37f2b1c641a9cd0f9bfac103491c51721cfc
Mark Sandusky <sandusky@uber.com>
2018-01-03 16:29:24 -0500

    Add namespaceID check in route

--- service/r2/routes.go
+++ service/r2/routes.go
@@ -65,10 +65,19 @@ func createNamespace(s *service, r *http.Request) (data interface{}, err error) 
 }
 
 func validateNamespace(s *service, r *http.Request) (data interface{}, err error) {
+	vars := mux.Vars(r)
 	var rsj ruleSetJSON
 	if err := parseRequest(&rsj, r.Body); err != nil {
 		return nil, err
 	}
+	if vars[namespaceIDVar] != rsj.Namespace {
+		return nil, fmt.Errorf(
+			"NamespaceID param: %s and RuleSet namespaceID: %s do not match",
+			vars[namespaceIDVar],
+			rsj.Namespace,
+		)
+	}
+
 	rss := rsj.ruleSetSnapshot()
 	if err := s.store.ValidateNamespace(rss); err != nil {
 		return nil, err
